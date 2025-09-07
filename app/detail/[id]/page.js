dfb5973cc902e8abcf394c1f2a1bf05765eee6e4
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2025-09-07 10:34:33 +0900

    database id toString Link

---
+++ app/detail/[id]/page.js
@@ -0,0 +1,16 @@
+import { connectDB } from "@/util/database";
+import { ObjectId } from "mongodb";
+
+export default async function Detail(props) {
+    const params = await props.params;
+    const db = (await connectDB).db(process.env.DB_NAME);
+    let result = await db.collection(process.env.COLLECTION_NAME).findOne({ _id: new ObjectId(params.id) });
+    // console.log(props);
+    return (
+        <div>
+            <h4>상세페이지</h4>
+            <h4>{result.title}</h4>
+            <p>{result.content}</p>
+        </div>
+    );
+}
--- app/list/page.js
+++ app/list/page.js
@@ -1,22 +1,26 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
 
 export default async function List() {
 
     const db = (await connectDB).db(process.env.DB_NAME);
     let result = await db.collection(process.env.COLLECTION_NAME).find().toArray();
-    // 데이터 가져오기
-    // console.log(result[0].title);
+    result.forEach(function (a) {
+        console.log(a._id.toString());
+    });
 
     return (
         <div className="list-bg">
             {result.map(function (a, i) {
                 return (
                     <div className="list-item" key={a._id.toString()}>
-                        <h4>{a.title}</h4>
+                        <Link href={"detail/" + a._id.toString()}>
+                            <h4>{a.title}</h4>
+                        </Link>
                         <p>{a.content}</p>
                     </div>
                 );
             })}
         </div>
     )
-} 
+}
